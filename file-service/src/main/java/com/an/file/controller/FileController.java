a67395db4326cfafb4a7927d7790a39a9473c0a2
AnNguyen05092004 <an05092004@gmail.com>
2026-04-01 09:36:14 +0700

    feat(file): store file in local storage

---
+++ file-service/src/main/java/com/an/file/controller/FileController.java
@@ -0,0 +1,28 @@
+package com.an.file.controller;
+
+import com.an.file.dto.ApiResponse;
+import com.an.file.service.FileService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class FileController {
+    FileService fileService;
+
+    @PostMapping("/media/upload")
+    ApiResponse<Object> uploadMedia(@RequestParam("file") MultipartFile file) throws IOException {
+        return ApiResponse.<Object>builder()
+                .message("File uploaded successfully")
+                .result(fileService.uploadFile(file))
+                .build();
+    }
+}
---
+++ file-service/src/main/java/com/an/file/service/FileService.java
@@ -0,0 +1,32 @@
+package com.an.file.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class FileService {
+    public Object uploadFile(MultipartFile file) throws IOException {
+        Path folder = Paths.get("/Users/an/IdeaProjects/bookteria/localstorage");
+        String fileExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+
+        String fileName = Objects.isNull(fileExtension)
+                ? UUID.randomUUID().toString()
+                : UUID.randomUUID().toString() + "." + fileExtension;
+
+        // Tạo đường dẫn file đầy đủ
+        Path filePath = folder.resolve(fileName).normalize().toAbsolutePath();
+        // Lưu file xuống disk
+        Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+        return null;
+    }
+}
